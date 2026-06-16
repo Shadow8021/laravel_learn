0ae6b7665129f3fe0f2926e91559e4c0520e7eea
Shadow8021 <gregoyaga@gmail.com>
2026-06-16 19:49:05 +0100

    update users route

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,13 @@ use Illuminate\Contracts\Support\Responsable;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/users/{id}',function($id){
-    $users =[
+ $users =[
     1=>["noms"=>"John doe", "email"=>"johndeo@gmail.fr","age"=>"15"],
     2=>["noms"=>"shadow", "email"=>"shadow@gmail.fr","age"=>"18"],
     3=>["noms"=>"oko", "email"=>"oko@gmail.fr","age"=>"19"],
 ];
+
+Route::get('/users/{id}',function($id) use($users){
+   
 return $users[$id];
 });
